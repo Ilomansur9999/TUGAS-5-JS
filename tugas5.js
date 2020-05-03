d9124e32620cb12b5831f5402b3d9d2ec1d9e70f
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 15:04:35 +0700

    TUGAS 5 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas5.js
@@ -0,0 +1,41 @@
+
+
+
+
+for(var i=1; i<=20; i++) {
+	
+	console.log(i * 3);
+	}
+
+
+
+
+// var i = 4;
+
+// while(i<=20){
+// 	console.log(i)
+// 	i++;
+// }
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas5.js
+// 3
+// 6
+// 9
+// 12
+// 15
+// 18
+// 21
+// 24
+// 27
+// 30
+// 33
+// 36
+// 39
+// 42
+// 45
+// 48
+// 51
+// 54
+// 57
+// 60
